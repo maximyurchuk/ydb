4d7c938d71e091e8167e33055ae885884da710f2
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-11-18 12:34:51 +0300

    crash

---
+++ junk/ut_crash/main.cpp
@@ -0,0 +1,11 @@
+#include <library/cpp/testing/common/env.h>
+#include <library/cpp/testing/unittest/registar.h>
+
+Y_UNIT_TEST_SUITE(Foo) {
+Y_UNIT_TEST(Bar)
+{
+    volatile int* p = 0;
+    *p = 42;
+}
+
+}
